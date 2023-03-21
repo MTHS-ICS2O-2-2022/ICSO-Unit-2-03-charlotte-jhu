6b27de9aff01e6e8ed55c85e53cdcd2f6ba329ef
charlotte-jhu <124389290+charlotte-jhu@users.noreply.github.com>
2023-03-21 17:08:44 +0000

    inital webpage code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Charlotte Jhu
+// Created on: March 2023
 // This file contains the JS functions for index.html
+
+/**
+ * .* This function is called when the button is clicked
+ */
+function myButtonClicked() {
+  // input
+  const streetNumber = document.getElementById("street-number").value
+  const streetName = document.getElementById("street-name").value
+
+  // output
+  document.getElementById("address").innerHTML = 
+  "Your adress is " + streetNumber + ", " + streetName + "."
+}
